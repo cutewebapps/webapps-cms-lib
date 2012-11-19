a101f0bdd80194b3800d513f0e7e7125171ba175
Victor Lopata <v.lopata@webcerebrium.com>
2012-11-19 18:19:12 -0500

    WC_ to CWA_

--- classes/Cms/ctrl/UploadCtrl.php
+++ classes/Cms/ctrl/UploadCtrl.php
@@ -27,7 +27,7 @@ class Cms_UploadCtrl extends App_AbstractCtrl
         if ( is_object( App_Application::getInstance()->getConfig()->cdn ) ) {
             $arrDirectories = App_Application::getInstance()->getConfig()->cdn->toArray();
         } else {
-            $arrDirectories = array( '' => WC_APPLICATION_DIR . '/cdn/upload' );
+            $arrDirectories = array( '' => CWA_APPLICATION_DIR . '/cdn/upload' );
         }
 
         $strDestination = $this->_getParam( 'destination', '' );
