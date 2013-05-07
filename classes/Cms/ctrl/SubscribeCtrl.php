80709e11431f2c89284fce757f443a488825da0b
Victor Lopata <v.lopata@webcerebrium.com>
2013-05-07 10:15:26 -0400

    check unique email on subsribe

--- classes/Cms/ctrl/SubscribeCtrl.php
+++ classes/Cms/ctrl/SubscribeCtrl.php
@@ -8,20 +8,23 @@ class Cms_SubscribeCtrl extends App_DbTableCtrl
 		if ( $this->_isPost() ) {
 			$arrErrors = array();
 
-			$this->_require( array(
-				'field' => 'subscribe_email', 'message' => Lang_Hash::get('Please provide your email' ),
-				'field' => 'subscribe_email', 'method' => 'email', 'message' => Lang_Hash::get('Please provide valid email' ),
-			));
+			$strEmail = trim( $this->_getParam( 'subscribe_email' ));
+                        
+                        if ( trim( $strEmail ) == '' ) {
+				array_push( $arrErrors, array( 'subscribe_email' => Lang_Hash::get('Please provide your email' )));
+                        } else if ( !Sys_String::isEmail( $strEmail ) ) {
+				array_push( $arrErrors, array( 'subscribe_email' => Lang_Hash::get('Please provide valid email' )));
+                        }
 			
 			if ( count( $arrErrors ) == 0 ) {
 				// check email is unique for this event
-				$objEmail = Cms_Subscribe::Table()->fetchByEmail( $this->_getParam( 'subscribe_email' ));
+				$objEmail = Cms_Subscribe::Table()->fetchByEmail( $strEmail );
 				if ( isset( $objEmail ) )  {
 					array_push( $arrErrors, array( Lang_Hash::get( 'This email was already registered' ) ) );
 				}
 			}
 
-			if ( count( $arrErrors ) > 0 ) { $this->view->arrError = $arrErrors(); return; }
+			if ( count( $arrErrors ) > 0 ) { $this->view->arrError = $arrErrors; return; }
 		}
 		parent::editAction();
 	}
